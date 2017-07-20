67a074d98c32ddd734f7e6a10ca5f5475e7352ed
Kei Ito <kei.itof@gmail.com>
2017-07-20 17:16:42 +0900

    keep data

--- index.js
+++ index.js
@@ -11,55 +11,53 @@ function plugin(params = {}) {
 
 	const filter = createFilter(params.include, params.exclude);
 	const classLabeler = new Labeler('classNames');
-	const globalRoot = postcss.root();
-
-	async function transform (source, id) {
-		if (!filter(id) || path.extname(id) !== '.css') {
-			return null;
-		}
-		const {root} = await postcss(params.postcss || []).process(source);
-		const labeled = {};
-		root.walkRules((rule) => {
-			const {selector} = rule;
-			let replaceCount = 0;
-			rule.selector = selector.replace(/\.([^,\s]+)/g, (match, className) => {
-				replaceCount++;
-				const label = classLabeler.label(`${id}${className}`);
-				const newClassName = `_${new BigNumber(label).toString(RADIX)}`;
-				labeled[className] = newClassName;
-				return `.${newClassName}`;
-			});
-			if (0 < replaceCount && params.debug) {
-				rule.before({text: selector});
-			}
-		});
-		globalRoot.append(...root.nodes);
-		return `export default ${JSON.stringify(labeled, null, '\t')};`;
-	}
-
-	function transformBundle(source) {
-		globalRoot.walk(({raws}) => {
-			for (const key of ['before', 'between', 'after']) {
-				const value = raws[key];
-				if (value) {
-					raws[key] = value.replace(/\s/g, '');
-				}
-			}
-		});
-		const labeler = new Labeler('css');
-		const encodedRules = globalRoot.nodes
-		.map((node) => {
-			return encodeString(`${node}`, labeler);
-		});
-		classLabeler.clear();
-		globalRoot.nodes.splice(0, globalRoot.nodes.length);
-		return `${source}\n${generageCode(labeler.items, encodedRules, params.debug)}`;
-	}
+	const cache = new Map();
 
 	return {
 		name: 'embed-css',
-		transform,
-		transformBundle
+		async transform (source, id) {
+			if (!filter(id) || path.extname(id) !== '.css') {
+				return null;
+			}
+			const {root} = await postcss(params.postcss || []).process(source);
+			const labeled = {};
+			root.walkRules((rule) => {
+				const {selector} = rule;
+				let replaceCount = 0;
+				rule.selector = selector.replace(/\.([^,\s]+)/g, (match, className) => {
+					replaceCount++;
+					const label = classLabeler.label(`${id}${className}`);
+					const newClassName = `_${new BigNumber(label).toString(RADIX)}`;
+					labeled[className] = newClassName;
+					return `.${newClassName}`;
+				});
+				if (0 < replaceCount && params.debug) {
+					rule.before({text: selector});
+				}
+			});
+			cache.set(id, root.nodes);
+			return `export default ${JSON.stringify(labeled, null, '\t')};`;
+		},
+		transformBundle(source) {
+			const root = postcss.root();
+			for (const [, nodes] of cache) {
+				root.append(...nodes);
+			}
+			root.walk(({raws}) => {
+				for (const key of ['before', 'between', 'after']) {
+					const value = raws[key];
+					if (value) {
+						raws[key] = value.replace(/\s/g, '');
+					}
+				}
+			});
+			const labeler = new Labeler('css');
+			const encodedRules = root.nodes
+			.map((node) => {
+				return encodeString(`${node}`, labeler);
+			});
+			return `${source}\n${generageCode(labeler.items, encodedRules, params.debug)}`;
+		}
 	};
 }
 module.exports = plugin;
