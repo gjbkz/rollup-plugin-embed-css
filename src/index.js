10fda32438947506e71a5648ea46ac44cdca705f
Kei Ito <kei.itof@gmail.com>
2018-01-09 17:38:39 +0900

    remove async/await

--- src/index.js
+++ src/index.js
@@ -22,94 +22,33 @@ function minify(node) {
 	return node;
 }
 
-function plugin(params = {}) {
+module.exports = function plugin(params = {}) {
 
 	const filter = createFilter(params.include, params.exclude);
 	const classLabeler = new Labeler();
 	const roots = new Map();
 	const cache = new Map();
 
-	async function load(id, givenSource) {
-		if (!givenSource && cache.has(id)) {
-			return cache.get(id);
-		}
-		const source = givenSource || await new Promise((resolve, reject) => {
-			fs.readFile(id, 'utf8', (error, data) => {
-				if (error) {
-					reject(error);
-				} else {
-					resolve(data);
-				}
-			});
-		});
-		const {root} = await postcss(params.postcss || []).process(source, {from: id});
-		const classNames = {};
-		const dependencies = new Map();
-		root.walkAtRules((node, index) => {
-			if (node.name === 'import') {
-				node.params.trim().replace(/^(['"])([^\s'"]+)\1\s*([^\s]*)$/, (match, quote, target, givenName) => {
-					const name = givenName || `$${index + 1}`;
-					dependencies.set(name, target);
-					node.remove();
-				});
-			}
-		});
-		const replacements = new Map();
-		for (const [name, target] of dependencies) {
-			const result = await load(path.join(path.dirname(id), target));
-			const {classNames: importedClassNames} = result;
-			for (const className of Object.keys(importedClassNames)) {
-				replacements.set(importedClassNames[className], `${name}.${className}`);
-			}
-		}
-		root.walkRules((rule) => {
-			const {selector} = rule;
-			let replaceCount = 0;
-			rule.selector = Array.from(replacements)
-			.reduce((selector, [to, from]) => {
-				return selector.split(from).join(`.${to}`);
-			}, selector)
-			.replace(/\.=?(-?[_a-zA-Z]+[_a-zA-Z0-9-]*)/g, (match, className) => {
-				if (match.startsWith('.=') || replacements.has(className)) {
-					return `.${className}`;
-				}
-				replaceCount++;
-				const label = classLabeler.label(`${id}${className}`);
-				const newClassName = `_${new BigNumber(label).toString(RADIX)}`;
-				classNames[className] = newClassName;
-				return `.${newClassName}`;
-			});
-			if (0 < replaceCount && params.debug) {
-				rule.before({text: selector.replace(/[\r\n]+/, ' ')});
-			}
-		});
-		roots.set(id, root);
-		const result = {
-			classNames,
-			dependencies,
-		};
-		cache.set(id, result);
-		return result;
-	}
-
 	return {
 		name: 'embed-css',
-		async transform(source, id) {
+		transform(source, id) {
 			if (!filter(id) || path.extname(id) !== '.css') {
 				return null;
 			}
-			const {classNames, dependencies} = await load(id, source);
-			const code = [
-				...Array.from(dependencies)
-				.map(([, target]) => {
-					return `import '${target}';`;
-				}),
-				`export default ${JSON.stringify(classNames, null, '\t')};`,
-			].join('\n');
-			return {
-				code,
-				map: {mappings: ''},
-			};
+			return load(id, source)
+			.then(({classNames, dependencies}) => {
+				const code = [
+					...Array.from(dependencies)
+					.map(([, target]) => {
+						return `import '${target}';`;
+					}),
+					`export default ${JSON.stringify(classNames, null, '\t')};`,
+				].join('\n');
+				return {
+					code,
+					map: {mappings: ''},
+				};
+			});
 		},
 		outro() {
 			const labeler = new Labeler();
@@ -125,5 +64,76 @@ function plugin(params = {}) {
 			return generateCode(labeler, encodedRules, params.debug);
 		},
 	};
-}
-module.exports = plugin;
+
+	function load(id, givenSource) {
+		if (!givenSource && cache.has(id)) {
+			return Promise.resolve(cache.get(id));
+		}
+		return Promise.resolve()
+		.then(() => givenSource || new Promise((resolve, reject) => {
+			fs.readFile(id, 'utf8', (error, data) => {
+				if (error) {
+					reject(error);
+				} else {
+					resolve(data);
+				}
+			});
+		}))
+		.then((source) => postcss(params.postcss || []).process(source, {from: id}))
+		.then(({root}) => {
+			const classNames = {};
+			const dependencies = new Map();
+			root.walkAtRules((node, index) => {
+				if (node.name === 'import') {
+					node.params.trim().replace(/^(['"])([^\s'"]+)\1\s*([^\s]*)$/, (match, quote, target, givenName) => {
+						const name = givenName || `$${index + 1}`;
+						dependencies.set(name, target);
+						node.remove();
+					});
+				}
+			});
+			const replacements = new Map();
+			return Promise.all(Array.from(dependencies).map(([name, target]) => {
+				return load(path.join(path.dirname(id), target))
+				.then((result) => {
+					const {classNames: importedClassNames} = result;
+					for (const className of Object.keys(importedClassNames)) {
+						replacements.set(importedClassNames[className], `${name}.${className}`);
+					}
+				});
+			}))
+			.then(() => {
+				root.walkRules((rule) => {
+					const {selector} = rule;
+					let replaceCount = 0;
+					rule.selector = Array.from(replacements)
+					.reduce((selector, [to, from]) => {
+						return selector.split(from).join(`.${to}`);
+					}, selector)
+					.replace(/\.=?(-?[_a-zA-Z]+[_a-zA-Z0-9-]*)/g, (match, className) => {
+						if (match.startsWith('.=') || replacements.has(className)) {
+							return `.${className}`;
+						}
+						replaceCount++;
+						const label = classLabeler.label(`${id}${className}`);
+						const newClassName = `_${new BigNumber(label).toString(RADIX)}`;
+						classNames[className] = newClassName;
+						return `.${newClassName}`;
+					});
+					if (0 < replaceCount && params.debug) {
+						rule.before({text: selector.replace(/[\r\n]+/, ' ')});
+					}
+				});
+				roots.set(id, root);
+				const result = {
+					classNames,
+					dependencies,
+				};
+				cache.set(id, result);
+				return result;
+			});
+		});
+	}
+
+};
+module.exports.Labeler = Labeler;
