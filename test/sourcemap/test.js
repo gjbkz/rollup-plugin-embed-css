0a9100f0cfd3293ec4f69c111915cad77cd735e2
Kei Ito <kei.itof@gmail.com>
2018-08-31 15:46:21 +0900

    uninstall magic-string

--- test/sourcemap/test.js
+++ test/sourcemap/test.js
@@ -8,25 +8,28 @@ const {runCode} = require('../util.js');
 t.test('sourcemap', (t) => {
     const formats = ['es', 'iife', 'umd'];
     for (const format of formats) {
-        t.test(format, async (t) => {
-            const directory = await afs.mkdtemp(path.join(os.tmpdir(), `-embedCSS-${format}-`));
-            await afs.cpr(__dirname, directory);
-            const input = path.join(directory, 'input.js');
-            const cssDest = path.join(directory, 'output.css');
-            const bundle = await rollup.rollup({
-                input,
-                plugins: [embedCSS({dest: cssDest})],
+        for (const embed of [false, true]) {
+            t.test(`${format} embed:${embed}`, async (t) => {
+                const directory = await afs.mkdtemp(path.join(os.tmpdir(), `-embedCSS-${format}-`));
+                await afs.cpr(__dirname, directory);
+                const input = path.join(directory, 'input.js');
+                const cssDest = embed ? undefined : path.join(directory, 'output.css');
+                const bundle = await rollup.rollup({
+                    input,
+                    plugins: [embedCSS({dest: cssDest})],
+                });
+                const result = await bundle.generate({
+                    sourcemap: true,
+                    format,
+                });
+                const {results} = runCode(result.code);
+                t.match(results, {
+                    foo: 'foofoo',
+                    properties: {color: 'red'},
+                });
+                t.ok(result.map.mappings.match(/[^;]/));
             });
-            const result = await bundle.generate({
-                sourcemap: true,
-                format,
-            });
-            const {results} = runCode(result.code);
-            t.match(results, {
-                foo: 'foofoo',
-                properties: {color: 'red'},
-            });
-        });
+        }
     }
     t.end();
 });
