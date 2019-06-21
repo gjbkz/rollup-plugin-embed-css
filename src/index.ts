f5705110cb4b2a8cb6dca9a1462d93a629ed6a54
Kei Ito <kei.itof@gmail.com>
2019-06-21 18:31:33 +0900

    feat: use esifycss

--- src/index.ts
+++ src/index.ts
@@ -4,31 +4,41 @@ import * as rollup from 'rollup';
 import * as pluginUtils from 'rollup-pluginutils';
 
 module.exports = (
-    options: esifycss.ISessionParameters & {
+    options: esifycss.ISessionOptions & {
         exclude: Array<string | RegExp> | string | RegExp | null,
     },
 ): rollup.Plugin => {
     const filter = pluginUtils.createFilter(options.include || './**/*.css', options.exclude);
-    options.include = [];
-    options.watch = false;
-    let session: esifycss.Session | undefined;
+    const session = new esifycss.Session({...options, include: [], watch: false});
+    const helperScriptPromise = session.outputHelperScript();
     return {
         name: 'embedCSS',
-        options({watch}) {
-            options.watch = Boolean(watch);
-            return null;
-        },
         async resolveId(id, importer) {
             const filePath = importer ? path.join(path.dirname(importer), id) : id;
             if (!filter(filePath)) {
                 return null;
             }
-            if (!session) {
-                session = new esifycss.Session(options);
-                await session.outputHelperScript();
-            }
+            await helperScriptPromise;
             await session.processCSS(filePath);
             return `${filePath}${session.configuration.ext}`;
+        },
+        async generateBundle(_options, bundle) {
+            for (const [, chunk] of Object.entries(bundle)) {
+                let {code} = chunk;
+                if (code) {
+                    const cssRanges = esifycss.extractCSSFromScript(code);
+                    const tokens = new Map<string, number>();
+                    for (const rule of cssRanges) {
+                        for (const token of esifycss.tokenizeString(rule.css)) {
+                            tokens.set(token, (tokens.get(token) || 0) + 1);
+                        }
+                    }
+                    const identifier = esifycss.createOptimizedIdentifier(tokens);
+                    code = esifycss.minifyCSSInScript(code, cssRanges, identifier);
+                    code = esifycss.setDictionary(code, identifier.idList);
+                    chunk.code = code;
+                }
+            }
         },
     };
 };
