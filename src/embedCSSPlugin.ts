c78419e485b9e1296e3ad98f4dcb0a963f4f8c49
Kei Ito <kei.itof@gmail.com>
2020-01-19 18:26:08 +0900

    refactor: fix eslint errors

--- src/embedCSSPlugin.ts
+++ src/embedCSSPlugin.ts
@@ -2,63 +2,100 @@ import * as path from 'path';
 import * as esifycss from 'esifycss';
 import * as rollup from 'rollup';
 import * as pluginUtils from 'rollup-pluginutils';
-import {updateBundle} from './updateBundle';
+import {updateBundleCSS, updateBundleScript} from './updateBundle';
 import {IPluginOptions} from './types';
 import {getFirstInput} from './getFirstInput';
+
+interface IPluginCore {
+    resolveId: rollup.ResolveIdHook,
+    load: rollup.LoadHook,
+    generateBundle: rollup.OutputPluginHooks['generateBundle'],
+}
+
+const getPluginCoreCSS = (
+    session: esifycss.Session,
+    filter: ReturnType<typeof pluginUtils.createFilter>,
+): IPluginCore => ({
+    resolveId: (importee) => importee === session.helperPath ? importee : null,
+    async load(id) {
+        if (id === session.helperPath) {
+            return 'export const addStyle = (rules) => console.log(rules);';
+        }
+        if (!filter(id)) {
+            return null;
+        }
+        return (await session.processCSS(id)).code;
+    },
+    generateBundle(_options, bundle) {
+        updateBundleCSS(bundle, session.configuration.output, this);
+    },
+});
+
+const getPluginCoreScript = (
+    session: esifycss.Session,
+    filter: ReturnType<typeof pluginUtils.createFilter>,
+): IPluginCore => ({
+    resolveId(importee, importer) {
+        const id = importer && !path.isAbsolute(importee) ? path.join(path.dirname(importer), importee) : importee;
+        return id === session.configuration.output.path ? session.helperPath : null;
+    },
+    async load(id) {
+        if (!filter(id)) {
+            return null;
+        }
+        return (await session.processCSS(id)).code;
+    },
+    generateBundle(_options, bundle) {
+        updateBundleScript(bundle);
+    },
+});
+
+const getPluginCoreDefault = (): IPluginCore => {
+    const throwError = () => {
+        throw new Error('NoSession');
+    };
+    return {
+        resolveId: throwError,
+        load: throwError,
+        generateBundle: throwError,
+    };
+};
 
 export const embedCSSPlugin = (
     options: IPluginOptions = {},
 ): rollup.Plugin => {
-    const filter = pluginUtils.createFilter(options.include || './**/*.css', options.exclude);
-    let session: esifycss.Session | undefined;
-    let helperId = '';
-    let cssMode = false;
+    let core = getPluginCoreDefault();
     return {
         name: 'embedCSS',
         options(inputOptions) {
-            const {css} = options;
             let {helper} = options;
-            if (!css && !helper) {
+            if (!options.css && !helper) {
                 const firstInput = getFirstInput(inputOptions.input);
                 if (firstInput) {
                     helper = path.join(path.dirname(firstInput), 'embedcss-helper.css.js');
                 }
             }
+            let helperId = '';
             if (helper) {
                 helperId = path.join(__dirname, `esifycss-helper${path.extname(helper || 's.js')}`);
             }
-            session = new esifycss.Session({...options, helper: helperId, css, include: [], watch: false});
-            cssMode = session.configuration.output.type === 'css';
+            const session = new esifycss.Session({...options, helper: helperId, include: [], watch: false});
+            const filter = pluginUtils.createFilter(options.include || './**/*.css', options.exclude);
+            if (session.configuration.output.type === 'css') {
+                core = getPluginCoreCSS(session, filter);
+            } else {
+                core = getPluginCoreScript(session, filter);
+            }
             return null;
         },
-        resolveId(importee, importer) {
-            if (!session) {
-                throw new Error('NoSession');
-            }
-            if (cssMode && importee === session.helperPath) {
-                return importee;
-            }
-            const id = importer && !path.isAbsolute(importee) ? path.join(path.dirname(importer), importee) : importee;
-            return id === helperId ? session.helperPath : null;
+        async resolveId(importee, importer) {
+            return await core.resolveId.call(this, importee, importer);
         },
         async load(id) {
-            if (!session) {
-                throw new Error('NoSession');
-            }
-            if (cssMode && id === session.helperPath) {
-                return 'export const addStyle = (rules) => console.log(rules);';
-            }
-            if (!filter(id)) {
-                return null;
-            }
-            const {code} = await session.processCSS(id);
-            return code;
+            return await core.load.call(this, id);
         },
-        generateBundle(_options, bundle) {
-            if (!session) {
-                throw new Error('NoSession');
-            }
-            updateBundle(bundle, session.configuration.output, this);
+        async generateBundle(options, bundle, isWrite) {
+            return await core.generateBundle.call(this, options, bundle, isWrite);
         },
     };
 };
--- src/updateBundle.ts
+++ src/updateBundle.ts
@@ -1,66 +1,58 @@
 import * as esifycss from 'esifycss';
 import * as rollup from 'rollup';
-import {isOutputChunk} from './isOutputChunk';
+import {parseBundle} from './parseBundle';
 
-export const updateBundle = (
+export const updateBundleCSS = (
     bundle: rollup.OutputBundle,
     output: esifycss.ISessionOutput,
     context: rollup.PluginContext,
-): void => {
-    const tokens = new Map<string, number>();
-    const chunks = Object.values(bundle)
-    .filter(isOutputChunk)
-    .map((chunk) => {
-        const cssRanges = esifycss.extractCSSFromScript(chunk.code);
-        for (const rule of cssRanges) {
-            for (const token of esifycss.tokenizeString(rule.css)) {
-                tokens.set(token, (tokens.get(token) || 0) + 1);
+) => {
+    const {chunks} = parseBundle(bundle);
+    const cssList: Array<string> = [];
+    for (const {chunk, cssRanges} of chunks) {
+        if (0 < cssRanges.length) {
+            let {code} = chunk;
+            for (let index = cssRanges.length; index--;) {
+                const range = cssRanges[index];
+                cssList[index] = range.css;
+                code = `${code.slice(0, range.start)}'ESIFYCSSPlugin${index}'${code.slice(range.end)}`;
             }
-        }
-        return {chunk, cssRanges};
-    });
-    if (output.type === 'script') {
-        const identifier = esifycss.createOptimizedIdentifier(tokens);
-        for (const {chunk, cssRanges} of chunks) {
-            let code = esifycss.minifyCSSInScript(chunk.code, cssRanges, identifier);
-            code = esifycss.setDictionary(code, identifier.idList);
+            let methodName = '';
+            code = code.replace(
+                /(\w+)\s*\(\s*\[\s*((?:,\s*)?'ESIFYCSSPlugin\d+')+\s*\]\s*\)\s*[;\r\n]+/g,
+                (_match, method) => {
+                    if (methodName) {
+                        if (methodName !== method) {
+                            throw new Error(`UnexpectedMethod: ${method} !== ${methodName}`);
+                        }
+                    } else {
+                        methodName = method;
+                    }
+                    return '';
+                },
+            );
+            if (!methodName) {
+                throw new Error('NoMethodToAddStyles');
+            }
+            code = code.replace(new RegExp(`const\\s+${methodName}\\s*=.*?[;\\r\\n]+`), '');
             chunk.code = code;
         }
-    } else {
-        const cssList: Array<string> = [];
-        for (const {chunk, cssRanges} of chunks) {
-            if (0 < cssRanges.length) {
-                let {code} = chunk;
-                for (let index = cssRanges.length; index--;) {
-                    const range = cssRanges[index];
-                    cssList[index] = range.css;
-                    code = `${code.slice(0, range.start)}'ESIFYCSSPlugin${index}'${code.slice(range.end)}`;
-                }
-                let methodName = '';
-                code = code.replace(
-                    /(\w+)\s*\(\s*\[\s*((?:,\s*)?'ESIFYCSSPlugin\d+')+\s*\]\s*\)\s*[;\r\n]+/g,
-                    (_match, method) => {
-                        if (methodName) {
-                            if (methodName !== method) {
-                                throw new Error(`UnexpectedMethod: ${method} !== ${methodName}`);
-                            }
-                        } else {
-                            methodName = method;
-                        }
-                        return '';
-                    },
-                );
-                if (!methodName) {
-                    throw new Error('NoMethodToAddStyles');
-                }
-                code = code.replace(new RegExp(`const\\s+${methodName}\\s*=.*?[;\\r\\n]+`), '');
-                chunk.code = code;
-            }
-        }
-        context.emitFile({
-            type: 'asset',
-            fileName: output.path,
-            source: cssList.join('\n'),
-        });
+    }
+    context.emitFile({
+        type: 'asset',
+        fileName: output.path,
+        source: cssList.join('\n'),
+    });
+};
+
+export const updateBundleScript = (
+    bundle: rollup.OutputBundle,
+): void => {
+    const {chunks, tokens} = parseBundle(bundle);
+    const identifier = esifycss.createOptimizedIdentifier(tokens);
+    for (const {chunk, cssRanges} of chunks) {
+        let code = esifycss.minifyCSSInScript(chunk.code, cssRanges, identifier);
+        code = esifycss.setDictionary(code, identifier.idList);
+        chunk.code = code;
     }
 };
