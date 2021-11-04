feb060b973940892265acd4385b3f21a3210a499
Kei Ito <kei.itof@gmail.com>
2021-11-04 22:03:20 +0900

    fix: deleteFiles

--- test/deleteFiles.ts
+++ test/deleteFiles.ts
@@ -1,8 +1,23 @@
 import * as fs from 'fs';
 import * as path from 'path';
 
+const isError = (input: unknown): input is {code: string} => {
+    if (typeof input === 'object' && input) {
+        return typeof (input as Record<string, unknown>).code === 'string';
+    }
+    return false;
+};
+
 export const deleteFiles = async (filePath: string) => {
-    const stats = await fs.promises.stat(filePath);
+    const stats = await fs.promises.stat(filePath).catch((error: unknown) => {
+        if (isError(error) && error.code === 'ENOENT') {
+            return null;
+        }
+        throw error;
+    });
+    if (!stats) {
+        return;
+    }
     if (stats.isDirectory()) {
         for (const name of await fs.promises.readdir(filePath)) {
             await deleteFiles(path.join(filePath, name));
